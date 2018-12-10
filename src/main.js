d37149ffb62849c036ba86756189d4b68540410b
lulzneko <lulzneko@riotz.works>
2018-12-10 13:06:09 +0900

    ✨ Introduce minimal layout and pages

---
+++ src/main.js
@@ -0,0 +1,6 @@
+import DefaultLayout from '~/layouts/default.vue';
+
+
+export default (Vue) => {
+  Vue.component('Layout', DefaultLayout);
+};
---
+++ src/pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <Layout>
+    <h1>Hello, world !!</h1>
+    <p>We are Riotz.works, a cheerful engineering team.</p>
+  </Layout>
+</template>
+
+
+<script>
+export default {
+  metaInfo: { titleTemplate: '%s' }
+};
+</script>
