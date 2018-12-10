645c91b8d7d1bf2be0e0528eb0676bf92005a9ce
lulzneko <lulzneko@riotz.works>
2018-12-10 13:21:19 +0900

    🔧 Configure settings of Gridsome to use Vuetify.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
+import Vuetify from 'vuetify';
 import DefaultLayout from '~/layouts/default.vue';
 
+import 'vuetify/dist/vuetify.min.css';
 
-export default (Vue) => {
+
+export default (Vue, { head }) => {
+  Vue.use(Vuetify);
+  head.link.push({ rel: 'stylesheet', href: '//fonts.googleapis.com/icon?family=Material+Icons' });
+
   Vue.component('Layout', DefaultLayout);
 };
