7e569a589971c7855f833012b89549b70715176a
lulzneko <lulzneko@riotz.works>
2018-12-28 21:12:44 +0900

    ✨ Introduce Works page

---
+++ contents/works/rap-tap-app/index.md
@@ -0,0 +1,25 @@
+---
+number: 1
+slug: rap-tap-app
+title: ラップ、タップ、アップ 🎶
+icon: ./images/icon.png
+image: ./images/screen-01.png
+url: https://riotz.works/rap-tap-app
+repos:
+  - https://github.com/riotz-works/rap-tap-app
+  - https://github.com/riotz-works/rap-tap-app-core
+engineers:
+  - lulzneko
+  - lopburny
+  - javaponny
+excerpt: "SPAJAM 2018 東京Ｄ予選、テーマ「音楽」 での モバイル アプリ ハッカソン 優秀賞 受賞作品。<br/><br/>
+競演者２名の動画中継 と 観戦者への同時配信を行うリアルタイムの動画バトル・アプリで、<br/>
+観戦者からライブ感あるフィードバックにより、競演者と観戦者が一体となって盛り上がれる演出が特徴。<br/>"
+---
+
+
+### 発表資料
+- [リアルタイムの競演と参加型観戦で音楽を最高に楽しむアプリ「ラップ、タップ、アップ」](https://riotz.works/slides/?2018-spajam-qualification) @SPAJAM 2018 東京D予選
+- [リアルタイム動画ラップ バトル アプリを短時間で作り上げた完全サーバーレスな秘技](https://riotz.works/slides/?2018-serverless-conf) @Serverlessconf Tokyo 2018
+- [Vue.js/Nuxt.js で 実現できた の リアルタイム動画ラップ バトル アプリ](https://riotz.works/slides/?2018-vue-fes-reject-con) @Vue Fes Japan 2018 Reject Conference
+- [フル Node.js エコシステム で 戦った モバイルアプリのハッカソン](https://riotz.works/slides/?2018-nodefest) @東京Node学園祭2018
---
+++ src/pages/works.vue
@@ -0,0 +1,125 @@
+<template>
+  <Layout>
+
+    <h1>Works</h1>
+    <p>{{ description }}</p>
+
+    <v-container fluid grid-list-xl>
+      <v-layout justify-space-around wrap>
+        <v-flex v-for="work in works" :key="work.node.id">
+
+          <h2><g-image :src="work.node.icon" class="icon" />{{ work.node.title }}</h2>
+
+          <p class="link">
+            <ul>
+              <li><a :href="work.node.url" class="url"><i class="material-icons">link</i><span>{{ work.node.url }}</span></a></li>
+              <li v-for="repo in work.node.repos" :key="repo"><a :href="repo" class="url"><i class="material-icons">library_books</i><span>{{ repo }}</span></a></li>
+            </ul>
+          </p>
+
+          <p v-html="work.node.excerpt" />
+
+          <h3>開発</h3>
+          <p>
+            <a v-for="engineer in work.node.engineers" :key="engineer" :href="'/engineers/#' + engineer" class="engineer" :title="engineer">
+              <v-avatar slot="activator" size="36px"><g-image :src="engineers[engineer].node.image" /></v-avatar>
+            </a>
+          </p>
+
+          <p v-html="work.node.content" />
+
+        </v-flex>
+        <v-flex v-for="work in works" :key="'empty-' + work.node.id" class="empty"><div /></v-flex>
+      </v-layout>
+    </v-container>
+
+  </Layout>
+</template>
+
+
+<page-query>
+query Works {
+
+  works: allwork {
+    totalCount
+    edges {
+      node {
+        id
+        number
+        slug
+        title
+        icon
+        image
+        url
+        repos
+        engineers
+        excerpt
+        content
+      }
+    }
+  }
+
+  engineers: allengineer {
+    totalCount
+    edges {
+      node {
+        id
+        account
+        image
+      }
+    }
+  }
+
+}
+</page-query>
+
+
+<script>
+export default {
+  data: () => ({
+    title: 'Works',
+    description: ''
+  }),
+   computed: {
+    works() { return this.$page ? this.$page.works.edges.sort((a, b) => a.node.number < b.node.number ? -1 : 1) : [] },
+    engineers() { return this.$page ? this.$page.engineers.edges.reduce((obj, engineer) => ({...obj, [engineer.node.account]: engineer}), {}) : [] }
+  },
+  metaInfo() {
+    return {
+      title: this.title,
+      meta: [
+        { key: 'description',          template: true, content: this.description },
+        { key: 'twitter:title',        template: true, content: this.title },
+        { key: 'twitter:description',  template: true, content: this.description },
+        { key: 'og:title',             template: true, content: this.title },
+        { key: 'og:description',       template: true, content: this.description },
+        { key: 'itemprop:name',        template: true, content: this.title },
+        { key: 'itemprop:description', template: true, content: this.description }
+      ]
+    }
+  }
+};
+</script>
+
+
+<style lang="scss" scoped>
+.icon {
+  width: 64px;
+}
+
+.link {
+  ul {
+    padding-left: 0;
+    li {
+      list-style: none;
+    }
+  }
+  .material-icons {
+    margin-right: 4px;
+  }
+}
+
+.engineer .v-avatar {
+  margin-right: 8px;
+}
+</style>
