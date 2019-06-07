5cd95247bb33417a26a77f9d40f6c9ab7dce6677
lulzneko <lulzneko@riotz.works>
2019-06-07 15:31:52 +0900

    Introduce a font swap with Google Fonts

--- src/main.js
+++ src/main.js
@@ -8,8 +8,8 @@ export default (Vue, { head }) => {
   Vue.use(Vuetify);
   head.link.push({ rel: 'stylesheet', href: '//fonts.googleapis.com/icon?family=Material+Icons' });
 
-  head.link.push({ rel: 'stylesheet', href: '//fonts.googleapis.com/css?family=Quicksand' });
-  head.link.push({ rel: 'stylesheet', href: '//fonts.googleapis.com/css?family=M+PLUS+Rounded+1c&amp;subset=japanese' });
+  head.link.push({ rel: 'stylesheet', href: '//fonts.googleapis.com/css?family=Quicksand&display=swap' });
+  head.link.push({ rel: 'stylesheet', href: '//fonts.googleapis.com/css?family=M+PLUS+Rounded+1c&display=swap&subset=japanese' });
   head.link.push({ rel: 'stylesheet', href: '//cdn.jsdelivr.net/gh/tonsky/FiraCode@1.206/distr/fira_code.min.css' });
   head.link.push({ rel: 'stylesheet', href: '//unicons.iconscout.com/release/v0.0.4/css/unicons.css' });
 
